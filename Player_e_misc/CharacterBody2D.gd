63e0611bce47b19d00c309fe2f77259ebc138190
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-03-30 01:02:55 -0300

    cm 1

--- Player_e_misc/CharacterBody2D.gd
+++ Player_e_misc/CharacterBody2D.gd
@@ -1,8 +1,5 @@
 extends CharacterBody2D
 
-
-#mudar de area2D para characterBody2D, ele tem argumentos como move_and_slide que ajudam 
-
 @export var speed = 500
 @export var acceleration = 500
 @export var friction = 100
@@ -13,13 +10,9 @@ func _physics_process(delta):
 	player_movement(delta)
 
 func get_input():
-	#documentação sugere isso. testar depois:
-	#var input_direction = Input.get_vector("left", "right", "up", "down")
-	#velocity = input_direction * speed
+
 	input.x = int(Input.is_action_pressed("Right")) - int(Input.is_action_pressed("Left"))
 	input.y = int(Input.is_action_pressed("Down")) - int(Input.is_action_pressed("Up"))
-	
-	#velocity = input * speed
 	
 	return input.normalized()
 	
@@ -35,12 +28,9 @@ func player_movement(delta):
 		velocity = velocity.limit_length(speed)
 	move_and_slide()
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
