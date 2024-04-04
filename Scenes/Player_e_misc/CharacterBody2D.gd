0214b8323bb431977ffe1380aa5abfa508c5ccd7
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-04-04 10:57:07 -0300

    sonar cooldown
    agora não dá pra spammar sonar e ele gasta energia. Ainda é possivel usar mesmo com menos de 0 En

--- Scenes/Player_e_misc/CharacterBody2D.gd
+++ Scenes/Player_e_misc/CharacterBody2D.gd
@@ -38,8 +38,8 @@ func _ready():
 	pass
 
 func _process(delta):
-	if energy <= 0:
-		energy = 0
+	#if energy <= 0:
+	#	energy = 0
 	#if Input.is_action_pressed("Right"):
 	#	energy -= 1
 	if Input.is_action_pressed("Sonar"):
@@ -49,13 +49,28 @@ func _on_energy_timer_timeout():
 	energy -= 1
 	
 func Sonar():
-	$Sonar.visible = true
-	var tween = create_tween()
-	tween.tween_property($Sonar, "scale", Vector2(20,20), 1)
-	tween.parallel().tween_property($Sonar, "energy", 0, 1.8)
+	if not sonar:
+		sonar = true
+		$"Sonar cooldown".start()
+		energy = energy - 10
+		$Sonar.visible = true
+		
+		var tween = create_tween()
+		tween.tween_property($Sonar, "scale", Vector2(20,20), 1)
+		tween.parallel().tween_property($Sonar, "energy", 0, 2)
+		
+		await tween.finished
+		$Sonar.visible = false
+		$Sonar.scale = Vector2(1,1)
+		$Sonar.energy = 1.0
+
 	
 
 	#tween.tween_callback($Sonar.queue_free)
 	
 	#search for "on finished" signal for the tween. it kinda works bit it only shrinks
 	
+
+
+func _on_sonar_cooldown_timeout():
+	sonar = false
