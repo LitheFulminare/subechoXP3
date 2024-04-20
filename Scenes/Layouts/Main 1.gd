c56fa38af90973f1c3582903d20966aa7e2b6da7
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-04-20 01:08:21 -0300

    spawn inimigo

--- Scenes/Layouts/Main 1.gd
+++ Scenes/Layouts/Main 1.gd
@@ -1,19 +1,28 @@
 extends Node2D
 
+@export var inimigo_scene : PackedScene
+
 func _ready():
-	pass 
-
-
-
+	pass
+	
 func _process(delta):
 	
 	$UI/Integridade.text = "Integridade: " + str($CanvasGroup/Player.life)
 	$UI/Energia.text = "Energia: " + str($CanvasGroup/Player.energy)
 	
+	
+	
 	if $CanvasGroup/Player.life <= 0 || $CanvasGroup/Player.energy <= 0 :
 		game_over()
-		
-
+	
+	
+	
 func game_over():
 	await get_tree().create_timer(3).timeout
 	get_tree().reload_current_scene()
+
+
+func _on_spawn_inimigo_timeout():
+	var inimigo = inimigo_scene.instantiate()
+	inimigo.position = $Marker2D.position
+	add_child(inimigo)
