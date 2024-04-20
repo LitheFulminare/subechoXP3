98900acbec98735eefd45a667d288941717826d5
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-04-20 00:30:51 -0300

    inimigo tomarr dano

--- Scenes/Inimigos/inimigo.gd
+++ Scenes/Inimigos/inimigo.gd
@@ -1,9 +1,23 @@
 extends CharacterBody2D
 
+@export var vida = 5
+@export var dano = 5
+
 const tiro1 = preload("res://Scenes/Player_e_misc/Particulas e projéteis/Tiro 1.tscn")
 
 func _ready():
 	$Sprite.play("default")
 	
 func _process(delta):
-	pass
+	if vida <= 0:
+		queue_free()
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("tiro"):
+		var danoRec = area.get_parent().dano
+		print("tipo")
+		danoTiro(danoRec)
+		
+func danoTiro(danoRec):
+	vida -= danoRec
--- Scenes/Player_e_misc/Particulas e projéteis/Tiro 1.gd
+++ Scenes/Player_e_misc/Particulas e projéteis/Tiro 1.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var dano = 3
 var speed = 3000
 
 var direction = Vector2.ZERO
