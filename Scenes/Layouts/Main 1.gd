232028a4df5e7eaf237376a57e244e88ae0475b3
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-04-22 01:30:13 -0300

    misc

--- Scenes/Layouts/Main 1.gd
+++ Scenes/Layouts/Main 1.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+#var fase2 = preload("res://Scenes/Layouts/Main 2.tscn")
+
 @export var inimigo_scene : PackedScene
 var inimigos_mortos = 0
 var meta_fase1 = 5
@@ -11,7 +13,7 @@ func _process(delta):
 	
 	$UI/Integridade.text = "Integridade: " + str($CanvasGroup/Player.life)
 	$UI/Energia.text = "Energia: " + str($CanvasGroup/Player.energy)
-	$"UI/Inimigos mortos".text = "inimigos_mortos: " + str(inimigos_mortos) + "/5"
+	$"UI/Inimigos mortos".text = "inimigos mortos: " + str(inimigos_mortos) + "/5"
 
 	if $CanvasGroup/Player.life <= 0 || $CanvasGroup/Player.energy <= 0:
 		game_over()
@@ -43,5 +45,6 @@ func contador_morte_inimigo():
 
 
 func _on_area_2d_area_entered(area):
-	if area.is_in_group("player"):
-		pass
+	#if inimigos_mortos >= meta_fase1:
+	#if area.is_in_group("player"):
+	get_tree().call_group("logica", "ir_para_fase_2")
---
+++ Scenes/Layouts/principal.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var level1 = preload("res://Scenes/Layouts/Main 1.tscn")
+var level2 = preload("res://Scenes/Layouts/Main 2.tscn")
+var debug = preload("res://Scenes/Layouts/debug.tscn")
+
+func ready():
+	pass
+	
+func _process(delta):
+	if Input.is_action_pressed("Iniciar"):
+		Iniciar()
+
+func Iniciar():
+	get_tree().change_scene_to_packed(level1)
+	
+func ir_para_fase_2():
+	get_tree().change_scene_to_packed(debug)
