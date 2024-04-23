f91a5441cdc1fde94ada3d0caeb9905b55eda64d
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-04-23 17:53:45 -0300

    misc

--- Scenes/Layouts/principal.gd
+++ Scenes/Layouts/principal.gd
@@ -1,19 +1,20 @@
 extends Control
 
-var level1 = preload("res://Scenes/Layouts/Main 1.tscn")
-var level2 = preload("res://Scenes/Layouts/Main 2.tscn")
-var debug = preload("res://Scenes/Layouts/debug.tscn")
+#var level1 = preload("res://Scenes/Layouts/Main 1.tscn")
+#var level2 = preload("res://Scenes/Layouts/Main 2.tscn")
+#var debug = preload("res://Scenes/Layouts/debug.tscn")
 
 func ready():
 	pass
 	
 func _process(delta):
-	if Input.is_action_pressed("Iniciar"):
-		Iniciar()
+	pass
+	#if Input.is_action_pressed("Iniciar"):
+		#Iniciar()
 	
 
-func Iniciar():
-	get_tree().change_scene_to_packed(level1)
+#func Iniciar():
+	#get_tree().change_scene_to_packed(level1)
 	
 func ir_para_fase_2():
 	pass
