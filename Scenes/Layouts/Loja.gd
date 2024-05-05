802970473356c9a637d3b63c64dba335902e8a41
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-05-05 01:28:45 -0300

    Update Loja.gd

--- Scenes/Layouts/Loja.gd
+++ Scenes/Layouts/Loja.gd
@@ -1,12 +1,12 @@
 extends Control
 
-var weapon_preview : int
-var weapon_selected : int
+var weapon_preview : int # a arma que o jogador vê
+var weapon_selected : int # a arma que o jogador tem selecionado
 
 func _ready():
 	
-	if player_vars.current_weapon == null:
-		weapon_preview = 1
+	if player_vars.current_weapon == null:  # se nenhuma arma tiver sido selecionada antes,
+		weapon_preview = 1                  # ele vai escolher a arma 1
 		player_vars.current_weapon = 1
 	else:
 		weapon_preview = player_vars.current_weapon
@@ -19,24 +19,11 @@ func _ready():
 		$"MarginContainer/Sprite/Arma1/Sprites 3".visible = true
 	elif weapon_preview == 4:
 		$"MarginContainer/Sprite/Arma1/Sprites 4".visible = true
-		#switch_weapon()
-		
-	
-#func _process(delta):
-	
-	
-	#if weapon_selected == 1:
-		#$"MarginContainer/Sprite/Arma1/Sprites 1".visible = true
-	#elif weapon_selected == 2:
-		#$"MarginContainer/Sprite/Arma1/Sprites 2".visible = true
-	#elif weapon_selected == 3:
-		#$"MarginContainer/Sprite/Arma1/Sprites 3".visible = true
-	#elif weapon_selected == 4:
-		#$"MarginContainer/Sprite/Arma1/Sprites 4".visible = true
-	
+
 
 func _on_right_pressed():
 	if weapon_preview < 4:
+		# esconde a sprite atual, muda a var do preview e chama a função que relaliza a troca de arma
 		$MarginContainer/Sprite/Arma1.get_child(weapon_preview-1).visible = false
 		weapon_preview += 1
 		switch_weapon()
@@ -44,6 +31,7 @@ func _on_right_pressed():
 
 func _on_left_pressed():
 	if weapon_preview > 1:
+		# esconde a sprite atual, muda a var do preview e chama a função que relaliza a troca de arma
 		$MarginContainer/Sprite/Arma1.get_child(weapon_preview-1).visible = false
 		weapon_preview -= 1
 		switch_weapon()
