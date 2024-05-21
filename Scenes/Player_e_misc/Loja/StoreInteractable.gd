e9c95fc54a7aab7cdc07f24de37812e40d604ab2
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-05-21 09:46:24 -0300

    progresso na loja

--- Scenes/Player_e_misc/Loja/StoreInteractable.gd
+++ Scenes/Player_e_misc/Loja/StoreInteractable.gd
@@ -3,7 +3,7 @@ extends StaticBody2D
 @export_enum("Recuperar vida", "Recuperar energia", "powerup" ) var interact_type: String
 
 
-@export_enum("Nenhum") var powerup_type : String = "Nenhum"
+@export_enum("+ vida", "+ energia") var powerup_type : String
 
 var player_in_area = false
 var depleted = false
@@ -16,22 +16,23 @@ func _input(event):
 		interact()
 
 func interact():
-	if not depleted && player_vars.current_scrap >= 70:
-		depleted = true
-		get_tree().call_group("player", "change_stat", "scrap", -70)
-		get_tree().call_group("player", "regen", interact_type)
-		
-		if interact_type == "Recuperar energia":
-			$Sprite.play("default") # fica travado no ultimo frame
+	if interact_type != null:
+		if not depleted && player_vars.current_scrap >= 70:
+			depleted = true
+			get_tree().call_group("player", "change_stat", "scrap", -70)
+			get_tree().call_group("player", "regen", interact_type)
+			
+			if interact_type == "Recuperar energia":
+				$Sprite.play("default") # fica travado no ultimo frame
 
 
-	#match interact_type:
-		#"Recuperar vida":
-			##get_tree().call_group("player", "regen", interact_type)
-			#player_vars.current_life = player_vars.max_life
-		#"Recuperar energia":
-			##get_tree().call_group("player", "regen", interact_type)
-			#player_vars.current_energy = player_vars.max_energy
+		#match interact_type:
+			#"Recuperar vida":
+				##get_tree().call_group("player", "regen", interact_type)
+				#player_vars.current_life = player_vars.max_life
+			#"Recuperar energia":
+				##get_tree().call_group("player", "regen", interact_type)
+				#player_vars.current_energy = player_vars.max_energy
 
 
 func _on_buy_area_area_entered(area):
