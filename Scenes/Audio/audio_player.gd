12359bbc0255cc049fbc818b19a3554a6a7898cc
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-04-24 14:32:19 -0300

    fade out both players
    now the secondary audio player also fades out

--- Scenes/Audio/audio_player.gd
+++ Scenes/Audio/audio_player.gd
@@ -18,7 +18,7 @@ func play_main_theme():
 	is_playing_music = true
 	secondary_audio_player.max_polyphony = 2
 	play_music(main_theme)
-	while is_playing:
+	while is_playing_music:
 		await get_tree().create_timer(22).timeout
 		secondary_audio_player.stream = main_theme
 		secondary_audio_player.play()
@@ -28,13 +28,18 @@ func stop_playing_with_fadeout(fadeout_time : float = 1):
 	# resets max polyphony to the normal value
 	secondary_audio_player.max_polyphony = 1
 	
-	# create tween for the volume
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, "volume_db", -60, fadeout_time)
-	#tween.tween_property(secondary_audio_player, "volume_db", 0, fadeout_time)
-	await tween.finished
+	# create tween for the volume of both main and secondary Audio Players
+	var tween1 = get_tree().create_tween()
+	var tween2 = get_tree().create_tween()
+	tween1.tween_property(self, "volume_db", -60, fadeout_time)
+	tween2.tween_property(secondary_audio_player, "volume_db", -60, fadeout_time)
+	# I don't know why, but I'm pretty sure I have to await both tweens even though
+	# their duration is the same
+	await tween1.finished
+	await tween2.finished
 	
 	# after the tween it stops the music and sets the volume back to normal
 	stop()
+	secondary_audio_player.stop()
 	volume_db = 0
-	#secondary_audio_player.stop()
+	secondary_audio_player.volume_db = 0
