12e567ccf2d67b611b1dd5e3156daf4d45748dce
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-04-24 14:02:18 -0300

    loop menu music

--- Scenes/Audio/audio_player.gd
+++ Scenes/Audio/audio_player.gd
@@ -2,6 +2,10 @@ extends AudioStreamPlayer
 
 const main_theme = preload("res://Audio/Soundtracks/Abyssal_Cleaner_Theme.ogg")
 
+@onready var secondary_audio_player = $"Secondary Audio Player"
+
+var is_playing : bool = false
+
 func play_music(music: AudioStream, volume = 0.0):
 	if stream == music:
 		return
@@ -11,4 +15,15 @@ func play_music(music: AudioStream, volume = 0.0):
 	play()
 
 func play_main_theme():
+	is_playing = true
+	secondary_audio_player.max_polyphony = 2
 	play_music(main_theme)
+	while is_playing:
+		await get_tree().create_timer(22).timeout
+		secondary_audio_player.stream = main_theme
+		secondary_audio_player.play()
+
+func stop_playing():
+	is_playing = false
+	secondary_audio_player.max_polyphony = 1
+	stop()
