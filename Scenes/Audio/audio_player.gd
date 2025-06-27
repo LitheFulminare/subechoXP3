f3bbfa2fccf2bac5db8db026d19f4a09c1ead824
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-06-27 16:52:35 -0300

    level 1 audacity project

--- Scenes/Audio/audio_player.gd
+++ Scenes/Audio/audio_player.gd
@@ -16,26 +16,9 @@ func play_music(music: AudioStream, volume = 0.0):
 	play()
 
 func play_main_theme():
-	is_playing_music = true
-	
-	secondary_audio_player.max_polyphony = 2
 	play_music(main_theme)
-	
-	# the file lasts almost 30 sec but the actual song lasts only 22 sec=
-	# this loops the song while letting the last note ring 
-	while is_playing_music:
-		await get_tree().create_timer(22).timeout
-		
-		# maybe there's a better solution, but if I don't check is_playing_music
-		# again it might cause an unwanted loop if the bool becomes false after
-		# starting the timer
-		if !is_playing_music:
-			return
-			
-		secondary_audio_player.stream = main_theme
-		secondary_audio_player.play()
 
-func stop_playing_with_fadeout(fadeout_time : float = 1):
+func stop_playing_with_fadeout(fadeout_time: float = 1):
 	is_playing_music = false
 	# resets max polyphony to the normal value
 	secondary_audio_player.max_polyphony = 1
