c9386f9b916b37b766674c0f8d48f589f2b3ab31
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-06-28 15:54:51 -0300

    changes to MusicManager
    there's no function that handles looping, now there's only one audio file that uses loop offset (from import properties) to play the part with reverb tail when looping

--- Scenes/Audio/music_manager.gd
+++ Scenes/Audio/music_manager.gd
@@ -1,12 +1,17 @@
 extends AudioStreamPlayer
 
-const main_theme_intro = preload("res://Audio/Soundtracks/Main Theme/Abyssal Cleaner Theme - intro.ogg")
-const main_theme_loop = preload("res://Audio/Soundtracks/Main Theme/Abyssal Cleaner Theme - loop.ogg")
+enum songs
+{
+	no_song,
+	main_theme,
+	level1,
+}
 
-const level1_intro: AudioStream = preload("res://Audio/Soundtracks/Level 1/Level 1 - intro.ogg")
-const level1_loop: AudioStream = preload("res://Audio/Soundtracks/Level 1/Level 1 - loop.ogg")
+const main_theme: AudioStream  = preload("res://Audio/Soundtracks/Main Theme/Abyssal Cleaner Theme.ogg")
 
-var current_song: AudioStream
+const level1: AudioStream = preload("res://Audio/Soundtracks/Level 1/Level 1.ogg")
+
+var current_song: songs = songs.no_song
 var queued_song: Callable
 
 # this is used to loop the song without cutting the last note and letting it ring
@@ -14,20 +19,6 @@ var queued_song: Callable
 @onready var secondary_audio_player = $"Secondary Audio Player"
 
 var is_playing_music: bool = false
-
-# I think this is causing clipping, I might go back to the old callable thing
-func _ready() -> void:
-	if !timer.timeout.is_connected(play_seamless_music):
-		timer.timeout.connect(play_seamless_music)
-
-func play_seamless_music() -> void:
-	play_music(current_song)
-
-func loop_seamlessly(music_with_tail: AudioStream) -> void:
-	timer.wait_time = stream.get_length()
-	timer.start()
-	
-	current_song = music_with_tail
 
 func play_music(music: AudioStream, volume = 0.0) -> void:
 	if stream == music:
@@ -37,13 +28,12 @@ func play_music(music: AudioStream, volume = 0.0) -> void:
 	volume_db = volume
 	play()
 
+# principal calls this and plays the intro again if it's playing the loop
 func play_main_theme():
-	play_music(main_theme_intro)
-	loop_seamlessly(main_theme_loop)
+	play_music(main_theme)
 	
 func play_level_1():
-	play_music(level1_intro)
-	loop_seamlessly(level1_loop)
+	play_music(level1)
 
 func stop_playing_with_fadeout(fadeout_time: float = 1):
 	is_playing_music = false
